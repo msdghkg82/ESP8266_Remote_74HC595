ea1ee0173a74fbcc067920f06b76cd246650943e
sdgh <chipsekachap@gmail.com>
2026-09-12 12:21:47 +0330

    7

--- src/Webserver_Manager.cpp
+++ src/Webserver_Manager.cpp
@@ -3,23 +3,33 @@
 #include "Config.h"
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPUpdateServer.h>
-//#include <ArduinoJson.h>
+#include <ArduinoJson.h>
 
 namespace Webserver_Manager
 {
     static ESP8266WebServer server(80);
     static ESP8266HTTPUpdateServer httpUpdater;
 
+    static void SendJsonResponse(int code, const String& stat, const String& msg)
+    {
+        JsonDocument doc;
+        doc["status"] = stat;
+        doc["message"] = msg;
+        String response;
+        serializeJson(doc, response);
+        server.send(code, "application/json", response);
+    }
+    
     static void handleRoot()
     {
-        server.send(200, "text/plain", "Hello from ESP8266 Web Server!");
+        SendJsonResponse(200, "OK", "Hello from ESP8266 Webserver.");
     }
 
     static void ConnectAPIs()
     {
         server.on("/", handleRoot);
         server.onNotFound([]() {
-            server.send(404, "text/plain", "404: Not Found");
+            SendJsonResponse(404, "ERROR", "404: Not Found");
         });
     }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include "Webserver_Manager.h"
 #include "WiFi_Manager.h"
-#include "Remote_Driver.h"
+#include "RF_Remote_Driver.h"
 #include "74HC595_Driver.h"
 
 // put function declarations here:
@@ -9,15 +9,15 @@
 
 void setup() {
   // put your setup code here, to run once:
-  WiFiManager::init();
-  WebserverManager::init();
-  RemoteDriver::init();
-  HC595Driver::init();
+  WiFi_Manager::init();
+  Webserver_Manager::init();
+  RF_Remote_Driver::init();
+  HC595_Driver::init();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  WebserverManager::handleClient();
+  Webserver_Manager::handleClient();
 }
 
 // put function definitions here:
